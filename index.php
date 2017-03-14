698572308e302069977518dcf30d179e326f71ef
ernixl <ernixl@yahoo.com>
2017-03-14 15:16:59 +0000

    wadwADFESFZSrdgter

--- index.php
+++ index.php
@@ -1,23 +1,4 @@
 
-<select>
-<?php
-/**
- * Created by PhpStorm.
- * User: 1615154
- * Date: 14/03/2017
- * Time: 14:29
-
-$i = 1;
-while($i < 100){
-
-    echo "<option>{$i}</option>";
-$i++;
-}*/
-?>
-
-
-
-</select>
 
 <?php
 
