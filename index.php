e5ca8988549f7823cc0765fa4bfda48dfd165e16
ernixl <ernixl@yahoo.com>
2017-03-14 15:14:43 +0000

    wadwADFESFZS

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
  * User: 1615154
  * Date: 14/03/2017
  * Time: 14:29
- */
+
 $i = 1;
 while($i < 100){
 
     echo "<option>{$i}</option>";
 $i++;
-}
+}*/
 ?>
 
 
@@ -23,16 +23,21 @@ $i++;
 
 
 
-$value = (rand(0,2));
-if($value == 0){
-    echo "Specs";
 
-}else if($value == 1){
-    echo "Sausage Rolls";
+while($i < 22){
+    $value = (rand(0,2));
+    if($value == 0){
+        echo "{$i} day , u get Specs";
 
-}else{
-    echo "Mugs";
+    }else if($value == 1){
+        echo "{$i} day , u get Sausage Rolls";
+
+    }else{
+        echo "{$i} day , u get Mugs";
+    }
+
+
 }
-
+ echo " No more Available anything anything!";
 
 ?>
